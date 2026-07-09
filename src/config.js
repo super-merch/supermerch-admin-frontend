daae5869dee8fc25e0d9a467c258c8d4ad05c560
Ansh Raiyani <ansh.inbweb25@gmail.com>
2026-07-10 04:55:55 +0530

    fix: correct formatting in API configuration object

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    api: {
+    api: { 
         API_URL:
             process.env.NODE_ENV === "production"
                 ? (process.env.REACT_APP_API_URL_PROD || "https://api.supermerch.com.au")
